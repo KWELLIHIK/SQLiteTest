fa4b9a203a8628f5af79497f57447b96a26a58c2
SergeantBulkin <bezdeneg.ya@yandex.ru>
2019-11-22 11:19:09 +0300

    Deleted unused code

--- app/src/main/java/com/test/sqlitetest/MainActivity.java
+++ app/src/main/java/com/test/sqlitetest/MainActivity.java
@@ -2,6 +2,7 @@ package com.test.sqlitetest;
 
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,81 +12,41 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import java.io.IOException;
 
-    Button btnAdd, btnRead, btnClear;
-    EditText etName, etEmail;
-
-    DBHelper dbHelper;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    //Переменные для работы с БД
+    private DatabaseHelper mDBHelper;
+    private SQLiteDatabase mDb;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnAdd = (Button) findViewById(R.id.btnAdd);
-        btnAdd.setOnClickListener(this);
+        mDBHelper = new DatabaseHelper(this);
+        try
+        {
+            mDBHelper.updateDataBase();
+        }catch (IOException ex)
+        {
+            throw new Error("UnableToUpdateDatabase");
+        }
 
-        btnRead = (Button) findViewById(R.id.btnRead);
-        btnRead.setOnClickListener(this);
-
-        btnClear = (Button) findViewById(R.id.btnClear);
-        btnClear.setOnClickListener(this);
-
-        etName = (EditText) findViewById(R.id.etName);
-        etEmail = (EditText) findViewById(R.id.etEmail);
-
-        dbHelper = new DBHelper(this);
+        try
+        {
+            mDb = mDBHelper.getWritableDatabase();
+        }catch (SQLException sqlex)
+        {
+            throw sqlex;
+        }
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View v)
+    {
 
-        String name = etName.getText().toString();
-        String email = etEmail.getText().toString();
-
-        SQLiteDatabase database = dbHelper.getWritableDatabase();
-
-        ContentValues contentValues = new ContentValues();
-
-
-        switch (v.getId()) {
-
-            case R.id.btnAdd:
-                contentValues.put(DBHelper.KEY_NAME, name);
-                contentValues.put(DBHelper.KEY_MAIL, email);
-
-                database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
-                Toast.makeText(getApplicationContext(), "Line added", Toast.LENGTH_SHORT).show();
-                Log.d("mLog", "Row added");
-                break;
-
-            case R.id.btnRead:
-                Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
-
-                if (cursor.moveToFirst()) {
-                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
-                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
-                    int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
-                    do {
-                        Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
-                                ", name = " + cursor.getString(nameIndex) +
-                                ", email = " + cursor.getString(emailIndex));
-                    } while (cursor.moveToNext());
-                } else
-                    Log.d("mLog","0 rows");
-
-                Toast.makeText(getApplicationContext(), "Lines read", Toast.LENGTH_SHORT).show();
-
-                cursor.close();
-                break;
-
-            case R.id.btnClear:
-                database.delete(DBHelper.TABLE_CONTACTS, null, null);
-                Log.d("mLog", "Base cleared");
-                Toast.makeText(getApplicationContext(), "Database cleared", Toast.LENGTH_SHORT).show();
-                break;
-        }
-        dbHelper.close();
     }
 }
