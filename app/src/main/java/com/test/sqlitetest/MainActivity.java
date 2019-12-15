ce69b276beda6a22605093f969bca70b935cd81b
SergeantBulkin <bezdeneg.ya@yandex.ru>
2019-12-15 17:54:02 +0300

    Added class Abonent to do Search in ArrayList. Search procces is unsuccessed.

--- app/src/main/java/com/test/sqlitetest/MainActivity.java
+++ app/src/main/java/com/test/sqlitetest/MainActivity.java
@@ -22,7 +22,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements MyAdapter.MyAdapterListener
 {
     //Переменные для работы с БД
     private DatabaseHelper mDBHelper;
@@ -31,8 +31,10 @@ public class MainActivity extends AppCompatActivity
     //RecyclerView
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
-    RecyclerView.Adapter adapter;
+    MyAdapter adapter;
 
+
+    //----------------------------------------------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -60,28 +62,35 @@ public class MainActivity extends AppCompatActivity
         }
         //Часть кода для RecyclerView
         //Наполнение списка данными
-        ArrayList<String> names = new ArrayList<>();
-        ArrayList<String> numbers = new ArrayList<>();
+        ArrayList<Abonent> abonents = new ArrayList<>();
 
-        Cursor cursor = mDb.rawQuery("SELECT user_profile._id, user_profile.user_name, abonent.mobile_number FROM  user_profile LEFT JOIN abonent ON abonent.user_id = user_profile._id;", null);
+        //Показывает всех абонентов
+        //Cursor cursor = mDb.rawQuery("SELECT user_profile._id, user_profile.user_name, abonent.mobile_number FROM  user_profile LEFT JOIN abonent ON abonent.user_id = user_profile._id;", null);
+
+        //Показывает только тех, у кого есть номер
+        Cursor cursor = mDb.rawQuery("SELECT user_profile._id, user_profile.user_name, abonent.mobile_number FROM  abonent LEFT JOIN user_profile ON user_profile._id = abonent.user_id;", null);
+
         cursor.moveToFirst();
 
+        //Добавление из базы в ArrayList
         while(!cursor.isAfterLast())
         {
+            Abonent abonent = new Abonent();
             if(cursor.isNull(1))
             {
-                names.add("Имя отсутствует");
+                abonent.setName("Имя отсутствует");
             }else
             {
-                names.add(cursor.getString(1));
+                abonent.setName(cursor.getString(1));
             }
             if(cursor.isNull(2))
             {
-                numbers.add("Номер остутствует");
+                abonent.setNumber("Номер остутствует");
             }else
             {
-                numbers.add(cursor.getString(2));
+                abonent.setNumber(cursor.getString(2));
             }
+            abonents.add(abonent);
             cursor.moveToNext();
         }
         cursor.close();
@@ -89,10 +98,10 @@ public class MainActivity extends AppCompatActivity
         recyclerView = findViewById(R.id.recyclerView);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        adapter = new MyAdapter(names, numbers);
+        adapter = new MyAdapter(abonents, this);
         recyclerView.setAdapter(adapter);
     }
-
+    //----------------------------------------------------------------------------------------------
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -101,13 +110,10 @@ public class MainActivity extends AppCompatActivity
 
         //getting the search view from the menu
         MenuItem searchViewItem = menu.findItem(R.id.menu_search);
-
         //getting search manager from systemservice
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-
         //getting the search view
         final SearchView searchView = (SearchView) searchViewItem.getActionView();
-
         searchView.setIconifiedByDefault(true);
         //here we will get the search query
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
@@ -116,18 +122,20 @@ public class MainActivity extends AppCompatActivity
             public boolean onQueryTextSubmit(String query)
             {
                 //do the search here
+                adapter.getFilter().filter(query);
                 return false;
             }
 
             @Override
-            public boolean onQueryTextChange(String newText)
+            public boolean onQueryTextChange(String query)
             {
+                adapter.getFilter().filter(query);
                 return false;
             }
         });
         return true;
     }
-
+    //----------------------------------------------------------------------------------------------
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item)
     {
@@ -144,5 +152,11 @@ public class MainActivity extends AppCompatActivity
                 break;
         }
         return true;
+    }
+    //----------------------------------------------------------------------------------------------
+    @Override
+    public void onAbonentSelected(Abonent abonent)
+    {
+        Toast.makeText(getApplicationContext(), "Выбран: " + abonent.getName() + ", " + abonent.getNumber(), Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/test/sqlitetest/MyAdapter.java
+++ app/src/main/java/com/test/sqlitetest/MyAdapter.java
@@ -3,24 +3,30 @@ package com.test.sqlitetest;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implements Filterable
 {
-    ArrayList<String> names;
-    ArrayList<String> numbers;
-
-    public MyAdapter(ArrayList<String> names, ArrayList<String> numbers)
+    private ArrayList<Abonent> abonents;
+    private ArrayList<Abonent> abonentsFiltered;
+    private MyAdapterListener listener;
+    //----------------------------------------------------------------------------------------------
+    public MyAdapter(ArrayList<Abonent> abonents, MyAdapterListener listener)
     {
-        this.names = names;
-        this.numbers = numbers;
+        this.abonents = abonents;
+        this.listener = listener;
+        this.abonentsFiltered = abonents;
     }
-
+    //----------------------------------------------------------------------------------------------
     @NonNull
     @Override
     public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
@@ -28,20 +34,21 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_item, viewGroup, false);
         return new ViewHolder(v);
     }
-
+    //----------------------------------------------------------------------------------------------
     @Override
     public void onBindViewHolder(@NonNull MyAdapter.ViewHolder viewHolder, int i)
     {
-        viewHolder.itemName.setText(names.get(i));
-        viewHolder.itemNumber.setText(numbers.get(i));
+        final Abonent abonent = abonentsFiltered.get(i);
+        viewHolder.itemName.setText(abonents.get(i).getName());
+        viewHolder.itemNumber.setText(abonents.get(i).getNumber());
     }
-
+    //----------------------------------------------------------------------------------------------
     @Override
     public int getItemCount()
     {
-        return names.size();
+        return abonents.size();
     }
-
+    //----------------------------------------------------------------------------------------------
     class ViewHolder extends RecyclerView.ViewHolder
     {
         private TextView itemName;
@@ -52,6 +59,61 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>
             super(itemView);
             itemName = itemView.findViewById(R.id.item_name);
             itemNumber = itemView.findViewById(R.id.item_number);
+
+            itemView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    listener.onAbonentSelected(abonentsFiltered.get(getAdapterPosition()));
+                }
+            });
         }
+    }
+    //----------------------------------------------------------------------------------------------
+    @Override
+    public Filter getFilter()
+    {
+        return new Filter()
+        {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence)
+            {
+                String charString = charSequence.toString();
+                if(charString.isEmpty())
+                {
+                    abonentsFiltered = abonents;
+                }else
+                {
+                    ArrayList<Abonent> filteredList = new ArrayList<>();
+                    for(Abonent row : abonents)
+                    {
+                        // name match condition. this might differ depending on your requirement
+                        // here we are looking for name or phone number match
+                        if(row.getName().toLowerCase().contains(charString.toLowerCase()) || row.getNumber().contains(charSequence))
+                        {
+                            filteredList.add(row);
+                        }
+                    }
+                    abonentsFiltered = filteredList;
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = abonentsFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults)
+            {
+                abonentsFiltered = (ArrayList<Abonent>) filterResults.values;
+                // refresh the list with filtered data
+                notifyDataSetChanged();
+            }
+        };
+    }
+    //----------------------------------------------------------------------------------------------
+    public interface MyAdapterListener
+    {
+        void onAbonentSelected(Abonent abonent);
     }
 }
